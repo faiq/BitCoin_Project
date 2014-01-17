8840c654772415104c4940878d916881f0c255fa
faiq <faiqrazarizvi@gmail.com>
2014-01-16 21:54:05 -0500

    "DRY out the test code in static_pages"

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
-
 describe "Static pages" do
+    let(:title) { "Ruby on Rails Tutorial Sample App" }
     describe "Home page" do
         it "should have the content 'Sample App'" do
             visit '/static_pages/home'
@@ -8,7 +8,7 @@ describe "Static pages" do
         end
         it "should have the right title" do 
             visit '/static_pages/home'
-            expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+            expect(page).to have_title("#{title} | Home")
         end 
     end
     describe "Help page" do
@@ -18,7 +18,7 @@ describe "Static pages" do
         end
         it "should have the right title" do 
             visit '/static_pages/help'
-            expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+            expect(page).to have_title("#{title} | Help")
         end 
 
     end
@@ -29,14 +29,14 @@ describe "Static pages" do
         end
         it "should have the right title" do 
             visit '/static_pages/about'
-            expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+            expect(page).to have_title("#{title} | About")
         end 
     end 
 
     describe "Contact page" do 
         it "should have the title 'Contact'" do 
             visit '/static_pages/contact'
-            expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact") 
+            expect(page).to have_title("#{title} | Contact") 
         end 
 
         it "should have the content 'Contact'" do 
